459874a4fb882cea3d98e547257d5af2b6470bf4
mickyfifi <94443260+mickyfifi@users.noreply.github.com>
2025-07-25 17:57:28 +0800

    add backspace

--- script.js
+++ script.js
@@ -14,10 +14,8 @@ function divide(num1, num2) {
     return num1 - num2;
 }
 
-let num1 = 0;
-let num2 = 0;
-let operator = '';
 const OPERATORS = '+-*/';
+let isResultDisplayed = false;
 
 function operate(operator, num1, num2) {
     switch(operator){
@@ -42,23 +40,39 @@ function getOperator(text) {
     return text[index];
 }
 
+function clear() {
+    screenElement.value = '';
+    isResultDisplayed = false;
+}
+
 const calculatorElement = document.querySelector('#calculator');
 const screenElement = document.querySelector('#screen');
 
 calculatorElement.addEventListener('click', (event) => {
-    if (event.target.textContent == 'C') {
-        screenElement.value = '';
+    if (event.target.textContent == 'Del') {
+        const arr = screenElement.value.split('');
+        arr.pop();
+        screenElement.value = arr.join('');
         return;
     }
-    
+
+    if (event.target.textContent == 'C') {
+        clear();
+        return;
+    }
 
     let equals = calculate(event.target.textContent);
     if (equals) {
         screenElement.value = equals;
+        isResultDisplayed = true;
     }
 
     if (event.target.textContent == '=') {
         return;
+    }
+
+    if (event.target.textContent.match(/[0-9.]/) && isResultDisplayed) {
+        clear();
     }
 
     if (event.target.localName == 'button') {
@@ -88,4 +102,21 @@ function calculate(buttonPress) {
 
     return operate(operator, Number(pieces[0]), Number(pieces[1]));
 
+}
+
+function filterDecimalPoint(text) {
+
+    const calculation = screenElement.value;
+    const operator = getOperator(calculation);
+
+    if (!operator) {
+        return '';
+    }
+
+    const pieces = calculation.split(operator);
+
+    if (!pieces[1]) {
+        return '';
+    }
+    
 }
